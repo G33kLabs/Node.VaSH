30a8339401a5d1b6b4645131f1222720493a49a9
G33K <delarueguillaume@gmail.com>
2012-09-04 02:42:08 +0200

    Remove some comments

--- app.js
+++ app.js
@@ -101,10 +101,8 @@ else {
     var app = express()
       , server = http.createServer(app) ;
 
-
     // Register CMS as an express middleware plugin
     express.vash = require('./libs/server/express.vash') ;
-    //  , opts = require('./config').defaults ;
 
     // Add session && router support 
     app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
@@ -114,99 +112,6 @@ else {
     app.use(express.methodOverride());
     app.use(express.favicon());  
     app.use(express.vash());
-
-    /*
-    app.use(express.static(current_path + opts.public )) ;
-
-    // -- Prepare config options
-    _.each(opts, function(val, key) {
-        if ( key == 'menus') {
-            _.each(val, function(v,k) {
-                if ( v.active ) v.active = 'active' ;
-                if ( ! v.url ) v.url = '/'+(v.name||'').toLowerCase()+'/'; 
-                val[k] = v ;
-            })
-        }
-        opts[key] = val ;
-    })
-
-    // Extend opts 
-    _.extend(opts, {
-        public_path: current_path + opts.public
-    }); 
-
-    // -- Serve home path
-    app.get('/', function(req, res, next) {
-        var _opts = _.extend({}, opts, {
-            version: Date.now()
-        }); 
-        util.pump(mu.compileAndRender(opts.public_path+'index.html', _opts), res);
-    }) ;  
-
-    // -- Load objects
-    var Objects = {
-        post: require('./Models/model.post').shared
-    } ;
-
-    // -- Bind CMS Routes
-    _.each(Objects, function(mod) {
-        app.get(mod.route, function(req, res, next) {
-
-            // Parse url params
-            var params = {} ; for ( var i in req.params ) params[i] = req.params[i] ;
-
-            // Create object
-            var Obj = new mod(params) ;
-
-            // Async treatment
-            async.series([
-                function(callback) {
-                    callback(null, true)
-                },
-                function(callback) {
-                    callback(null, true)
-                }
-            ], 
-
-            // Compile template with view and display html
-            function(err, success) {
-                var _opts = _.extend({}, opts, {
-                    page: Obj.toJSON()
-                }) ;
-                var tplFile = opts.public_path+(_opts.page.template?_opts.page.template:'index.html') ;
-                util.pump(mu.compileAndRender(tplFile, _opts), res);
-            })
-
-        }) ;      
-    })
-
-    // -- Serve admin
-    var private_path = current_path + opts.private;
-    app.get('/admin/*', function(req, res, next) {
-        var _opts = _.extend({}, opts, {
-            version: Date.now(),
-            title: 'Admin | '+opts.title,
-            brand: 'Admin | '+opts.brand
-        }); 
-        util.pump(mu.compileAndRender(private_path+'index.html', _opts), res);
-    }) ;  
-
-    // -- Survey changes on home page to clear cache
-    watch.createMonitor(opts.public_path, function (monitor) {
-        monitor.on("created", function (f, stat) {
-            tools.debug(' [*] '+(cluster.isMaster?'M':cluster.worker.id)+' | '+json(stat)+' | '+json(f));
-        })
-        monitor.on("changed", function (f, curr, prev) {
-            tools.warning(' [*] '+(cluster.isMaster?'M':cluster.worker.id)+' | Mustache JS cache cleared !');
-            mu.clearCache();
-        })
-        monitor.on("removed", function (f, stat) {
-            tools.debug(' [*] '+(cluster.isMaster?'M':cluster.worker.id)+' | '+json(stat)+' | '+json(f));
-        })
-    } ) 
-
-
-*/
 
     // -- Start Server
     server.listen(config.server.port);

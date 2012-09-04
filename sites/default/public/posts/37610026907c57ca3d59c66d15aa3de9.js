a8b53963310e412a9d802ee065c353fabe741329
G33K <delarueguillaume@gmail.com>
2012-09-04 05:09:24 +0200

    Create a new post

---
+++ sites/default/public/posts/37610026907c57ca3d59c66d15aa3de9.js
@@ -0,0 +1,9 @@
+module.exports = {
+	id: '37610026907c57ca3d59c66d15aa3de9',
+	created: 1346680883077,
+	title: "Install Redis.io on Mac OSX as service",
+	desc: "How to install Redis.io on your Mac ?",
+	content: "37610026907c57ca3d59c66d15aa3de9.md",
+	tags: ['Redis.io', 'Mac OSX', 'Install', 'Node.js'],
+	author: 'delarueguillaume@gmail.com'
+}

9b5686d17b499c3f1965b5e1ba7791b2f0b29830
G33K <delarueguillaume@gmail.com>
2012-09-06 14:34:30 +0200

    Update post

--- sites/default/posts/37610026907c57ca3d59c66d15aa3de9.js
+++ sites/default/posts/37610026907c57ca3d59c66d15aa3de9.js
@@ -1,7 +1,7 @@
 module.exports = {
 	id: '37610026907c57ca3d59c66d15aa3de9',
 	created: 1346680883077,
-	title: "Install Redis.io on Mac OSX as service",
+	title: "Install Redis.io 2.4.17 on Mac OSX as service",
 	desc: "How to install Redis.io on your Mac ?",
 	content: "37610026907c57ca3d59c66d15aa3de9.md",
 	tags: ['Redis.io', 'Mac OSX', 'Install', 'Node.js'],

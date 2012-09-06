da60805ef4839bff0645a7d15bbcb76200b1886a
G33K <delarueguillaume@gmail.com>
2012-09-06 14:17:57 +0200

    Add a post

--- sites/default/posts/39cb4143882aede6d3395f5ab5dcb09c.js
+++ sites/default/posts/39cb4143882aede6d3395f5ab5dcb09c.js
@@ -1,7 +1,7 @@
 module.exports = {
 	id: '39cb4143882aede6d3395f5ab5dcb09c',
 	title: "Install Node.VaSH with NGINX",
-	created: 1346841808350,
+	created: 1346933863093,
 	author: 'delarueguillaume@gmail.com',
 	desc: "How to serve a node.js application in general ?",
 	content: "39cb4143882aede6d3395f5ab5dcb09c.md",
